45ba37f8cd522e3683835f54a64034d0f053553f
garrettwong99 <garrettwong99@gmail.com>
2025-01-29 12:08:14 -0600

    get links onto the infinite webpage

--- goodnewssold/news/utils.py
+++
@@ -1,6 +0,0 @@
-import feedparser
-
-def fetch_rss_feed(url):
-    """Fetch and parse the RSS feed from the given URL."""
-    feed = feedparser.parse(url)
-    return feed  # Contains feed entries and metadata
--- goodnewssold/news/views.py
+++ goodnewssold/news/views.py
@@ -1,7 +1,21 @@
+import feedparser
 from django.shortcuts import render
 from .models import Source
 
 def home(request):
     """Display a list of news articles."""
     sources = Source.objects.all()  # Fetch all RSS feed sources
-    return render(request, 'news/home.html', {'sources': sources})
+
+    articles = []
+    for source in sources:
+        feed = feedparser.parse(source.rss_url)
+        for entry in feed.entries:
+            articles.append({
+                'title': entry.title,
+                'link': entry.link,
+            })
+    
+    context = {
+        'articles': articles
+    }
+    return render(request, 'news/home.html', context)
